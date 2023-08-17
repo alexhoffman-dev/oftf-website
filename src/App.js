64e4036c04bd3d996b229ffeca7319bfeba3e8eb
TellemBigAlsentya <alexhoffman26@gmail.com>
2023-08-17 14:27:28 -0700

    assets added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './assets/oftf-logo-white.png'
+import background from './assets/ferns.png'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="background-image">
+        <img src={ background } alt="Background" />
+      </div>
+      <div className="logo-container">
+        <img src={ logo } alt="Logo" className="logo" />
+      </div>
+      <div className="button-container">
+        <button className="score-button">SCORE MY RIDE</button>
+      </div>
     </div>
   );
 }
